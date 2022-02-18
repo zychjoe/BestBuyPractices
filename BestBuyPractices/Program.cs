1a50daf29d6c623acc9fea120c95236221682606
Joe Zedd <zychjoe@gmail.com>
2022-02-17 20:48:55 -0500

    initial commit

---
+++ BestBuyPractices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
